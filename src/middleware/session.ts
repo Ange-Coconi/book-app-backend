41cace5d73af60980de73fd8c55fe2b81b87f091
Your Name <you@example.com>
2025-01-22 23:28:32 +0100

    redis store

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -3,23 +3,26 @@ import { RedisStore } from "connect-redis";
 import session from 'express-session';
 import { createClient } from 'redis';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-// let redisClient = createClient();
-// redisClient.connect().catch(console.error);
 
-// // Initialize store.
-// let redisStore = new RedisStore({
-//   client: redisClient,
-//   prefix: "myapp:",
-// })
+let redisClient = createClient({
+  url: process.env.REDIS_URL
+});
+
+redisClient.connect().catch(console.error);
+
+// Initialize store.
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "myapp:",
+})
 
 const sessionOptions: SessionOptions = {
   secret: 'eS9JK6&cTPs8jY@8Rw!4',
   resave: false,
   saveUninitialized: false,
-  store: new session.MemoryStore(),
+  store: redisStore,
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
