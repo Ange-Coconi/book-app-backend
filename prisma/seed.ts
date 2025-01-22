461eb1fdfd482e375fd12bed17802fbb47a79f3e
Your Name <you@example.com>
2025-01-22 14:34:03 +0100

    guess password updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -21,7 +21,7 @@ async function main() {
   });
 
   // Create user 1 (unauthenticated user)
-  const hashedPassword1 = await bcrypt.hash('guest_password', 10);
+  const hashedPassword1 = await bcrypt.hash(process.env.GUEST_PASSWORD!, 10);
   const user_1 = await prisma.user.upsert({
     where: { id: 1 },
     update: {},
